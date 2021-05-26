838bcd4b836fb5ca42239975b0f800f0d0dfd27a
Anders <and3k5@users.noreply.github.com>
2021-05-26 06:42:51 +0200

    Updates

--- wasm/build-wasm.sh
+++ wasm/build-wasm.sh
@@ -1,2 +1,2 @@
 docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) \
-  emscripten/emsdk emcc --no-entry -O1 iterate.c -o iterate.wasm -s WASM=1 -s "EXPORTED_FUNCTIONS=['_getIteration','_getIterations']"
+  emscripten/emsdk emcc --no-entry -O3 iterate.c -o iterate.wasm -s WASM=1 -s "EXPORTED_FUNCTIONS=['_getIteration','_getIterations']"
--- wasm/iterate.c
+++ wasm/iterate.c
@@ -1,29 +1,25 @@
-int getIteration(int width, int height, int x, int y, int maxIteration)
+int getIteration(const int width, const int height, const int x, const int y, const int maxIteration)
 {
     int iteration = 0;
     double i = 0;
     double j = 0;
+    const double w = width;
+    const double h = height;
     while (iteration++ < maxIteration)
     {
-        double a = i * i - j * j + ((x << 1) - 1.5 * ((double)width)) / ((double)width);
-        j = 2.0 * i * j + ((y << 1) - ((double)height)) / ((double)height);
+        const double a = i * i - j * j + ((x << 1) - 1.5 * (w)) / (w);
+        j = 2.0 * i * j + ((y << 1) - (h)) / (h);
         i = a;
         if (i * i + j * j > 4.0)
-        {
             break;
-        }
     }
     return iteration;
 }
 
-void getIterations(int *result, int width, int height, int maxIteration)
+void getIterations(int *result, const int width, const int height, const int maxIteration)
 {
     int pos = 0;
     for (int x, y = 0; y < height; y++)
-    {
         for (x = 0; x < width; x++)
-        {
             result[pos++] = getIteration(width, height, x, y, maxIteration);
-        }
-    }
 }
