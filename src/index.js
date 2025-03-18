6e08f5f53c8436ef84e4dd8915ccc02358e55f22
Anders <and3k5@users.noreply.github.com>
2025-03-18 22:19:00 +0100

    Remove "option"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as colorMaps from "./colormaps"
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as colorMaps from "./colormaps";
 import * as methods from "./methods";
 
 const colorSelector = document.querySelector("#colormap");
@@ -13,36 +13,30 @@ for (const colorMap of colorMaps.all) {
 
 colorSelector.options[0].selected = true;
 
-const methodSelector = document.querySelector("select#method");
-
-for (const method of methods.all) {
-    const option = new Option();
-    option.value = method.name;
-    option.textContent = method.label;
-    methodSelector.add(option);
-}
-
-methodSelector.options[0].selected = true;
-
 var canvas = document.querySelector("canvas");
 
-
-import { run } from './methods';
-import { renderCanvasElement } from './render';
+import { run } from "./methods";
+import { renderCanvasElement } from "./render";
 
 document.querySelector("button#submit").addEventListener("click", async () => {
     const WIDTH = document.querySelector("input#width").value;
     const HEIGHT = document.querySelector("input#height").value;
     const MAXITER = document.querySelector("input#maxIter").value;
     const COLORMAP = colorMaps.getColorMapByName(colorSelector.value);
-    const METHOD = methodSelector.value;
 
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
     console.debug("run");
     const start = performance.now();
-    const iterations = await run(METHOD, WIDTH, HEIGHT, MAXITER, COLORMAP);
+    const iterations = await run(WIDTH, HEIGHT, MAXITER, COLORMAP);
     const stop = performance.now();
     console.debug("Calculation took " + Math.round(stop - start) + "ms");
-    renderCanvasElement({ element: canvas, width: WIDTH, height: HEIGHT, colorMap: COLORMAP, iterations: iterations, maxIterations: MAXITER });
+    renderCanvasElement({
+        element: canvas,
+        width: WIDTH,
+        height: HEIGHT,
+        colorMap: COLORMAP,
+        iterations: iterations,
+        maxIterations: MAXITER,
+    });
 });
--- src/methods/index.js
+++ src/methods/index.js
@@ -1,20 +1,7 @@
 import * as wasm from "./wasm";
 
-export const all = [
-    {
-        name: "wasm",
-        label: "WebAssembly",
-        execute: wasm.execute,
-    },
-];
-
-function getMethodByName(name) {
-    return all.find((m) => m.name === name);
-}
-
-export async function run(METHOD, WIDTH, HEIGHT, MAXITER) {
-    var method = getMethodByName(METHOD);
-    var iterations = method.execute(WIDTH, HEIGHT, MAXITER);
+export async function run(WIDTH, HEIGHT, MAXITER) {
+    var iterations = wasm.execute(WIDTH, HEIGHT, MAXITER);
     if (iterations instanceof Promise) {
         iterations = await iterations;
     }
