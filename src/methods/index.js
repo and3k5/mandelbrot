0a7de7001615400ff224c875c6d58bb5e4553c5e
Anders <and3k5@users.noreply.github.com>
2025-03-18 22:08:48 +0100

    Remove old methods

--- src/methods/index.js
+++ src/methods/index.js
@@ -1,46 +1,15 @@
-
-import * as script from "./script";
-import * as worker from "./worker";
 import * as wasm from "./wasm";
-// import * as wasm2 from "./wasm-2";
-// import * as wasmWorker from "./wasm-worker";
-// import * as wasm2Worker from "./wasm-2-worker";
 
 export const all = [
-    {
-        name: "script",
-        label: "Script",
-        execute: script.execute,
-    },
-    {
-        name: "worker",
-        label: "Web worker",
-        execute: worker.execute,
-    },
     {
         name: "wasm",
         label: "WebAssembly",
         execute: wasm.execute,
     },
-    // {
-    //     name: "wasm-2",
-    //     label: "WebAssembly 2",
-    //     execute: wasm2.execute,
-    // },
-    // {
-    //     name: "wasm-worker",
-    //     label: "WebAssembly with worker",
-    //     execute: wasmWorker.execute,
-    // },
-    // {
-    //     name: "wasm-worker-2",
-    //     label: "WebAssembly 2 with worker",
-    //     execute: wasm2Worker.execute,
-    // }
-]
+];
 
 function getMethodByName(name) {
-    return all.find(m => m.name === name);
+    return all.find((m) => m.name === name);
 }
 
 export async function run(METHOD, WIDTH, HEIGHT, MAXITER) {
--- src/methods/script.js
+++
@@ -1,6 +0,0 @@
-import { getIterations } from '../calculate/iterate';
-
-export function execute(width, height, maxIteration) {
-    var iterations = getIterations({ width: width, height: height, maxIteration: maxIteration });
-    return iterations;
-}
